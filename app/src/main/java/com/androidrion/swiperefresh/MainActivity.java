cbe00996b16af66a4900975aca63b721f138421c
rionymous_ <callmerion24@gmail.com>
2019-10-21 17:05:24 +0700

    Initial commit

---
+++ app/src/main/java/com/androidrion/swiperefresh/MainActivity.java
@@ -0,0 +1,28 @@
+package com.androidrion.swiperefresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipe_to_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+            }
+        });
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, R.color.colorAccent, R.color.colorPrimaryDark);
+    }
+
+    public void fetchTimelineAsync(int page) {
+        //
+    }
+}
